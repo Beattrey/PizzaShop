ecfde7120824a160bfb743482a2f16cbe50abf71
beattrey <beattrey1@gmail.com>
2021-10-01 17:52:13 +0300

    add migration

--- app.rb
+++ app.rb
@@ -2,7 +2,21 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sinatra/activerecord'
+
+set :database, { adapter: "sqlite3", database: "pizzashop.db" }
+
+class Product < ActiveRecord::Base
+end
 
 get '/' do
-	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+  erb "Hello!"
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/contacts' do
+  erb :contacts
 end
